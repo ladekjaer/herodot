61209756debabe463ef96e70f250980e42100bab
Rasmus Ladekjær Pedersen <ladekjaer@gmail.com>
2026-02-17 10:23:18 +0100

    Update test data

--- database/initialization/02-test_data.dml.sql
+++ database/initialization/02-test_data.dml.sql
@@ -1,2 +1,2 @@
 INSERT INTO auth.users (id, username, password)
-VALUES (uuidv4(), 'rlad', 'password');
+VALUES ('671bea95-1949-40c1-a0a6-8b233fdaafd5', 'rlad', '$argon2id$v=19$m=19456,t=2,p=1$1RQOgJaikWV9ipGnqSMHKw$T/TbGWAOpGTEbLB1qdk+F56/M57HrA5sAZ4/DbF+Ucw');
