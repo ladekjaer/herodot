76d1f124bdeccba0aefbc218150ac0dde6676e7b
Rasmus Ladekjær Pedersen <ladekjaer@gmail.com>
2026-03-04 10:43:06 +0100

    Formatting

--- src/repository.rs
+++ src/repository.rs
@@ -1,12 +1,12 @@
+use crate::authentication::api_key::ApiKey;
+use crate::authentication::user::User;
+use rerec::Reading;
 use rerec::bme280::BME280;
 use rerec::ds18b20::DS18B20;
-use crate::authentication::user::User;
 use rerec::record::Record;
-use rerec::Reading;
-use sqlx::types::chrono;
 use sqlx::PgPool;
+use sqlx::types::chrono;
 use uuid::Uuid;
-use crate::authentication::api_key::ApiKey;
 
 const DEFAULT_LIMIT: usize = 100;
 const HARD_LIMIT: usize = 5000;
@@ -26,7 +26,11 @@ pub(crate) struct Ds18b20Record {
 
 impl From<Ds18b20Record> for Record {
     fn from(value: Ds18b20Record) -> Self {
-        Record::new(value.id, value.timestamp, Reading::DS18B20(DS18B20::new(value.device_name, value.raw_reading)))
+        Record::new(
+            value.id,
+            value.timestamp,
+            Reading::DS18B20(DS18B20::new(value.device_name, value.raw_reading)),
+        )
     }
 }
 
@@ -41,7 +45,15 @@ pub(crate) struct Bme280Record {
 
 impl From<Bme280Record> for Record {
     fn from(value: Bme280Record) -> Self {
-        Record::new(value.id, value.timestamp, Reading::BME280(BME280::new(value.temperature, value.pressure, value.humidity)))
+        Record::new(
+            value.id,
+            value.timestamp,
+            Reading::BME280(BME280::new(
+                value.temperature,
+                value.pressure,
+                value.humidity,
+            )),
+        )
     }
 }
 
@@ -89,15 +101,18 @@ impl Repository {
         }
     }
 
-    pub(crate) async fn get_record_by_id(&self, record_id: Uuid) -> Result<Option<Record>, sqlx::Error> {
+    pub(crate) async fn get_record_by_id(
+        &self,
+        record_id: Uuid,
+    ) -> Result<Option<Record>, sqlx::Error> {
         let bme280 = self.get_bme280_record_by_id(record_id).await?;
         if let Some(bme280) = bme280 {
-            return Ok(Some(Record::from(bme280)))
+            return Ok(Some(Record::from(bme280)));
         }
 
         let ds18b20 = self.get_ds18b20_record_by_id(record_id).await?;
         if let Some(ds18b20) = ds18b20 {
-            return Ok(Some(Record::from(ds18b20)))
+            return Ok(Some(Record::from(ds18b20)));
         }
 
         Ok(None)
@@ -108,28 +123,25 @@ impl Repository {
         let bme280_records = self.get_all_bme280_records().await?;
         let ds18b20_records = self.get_all_ds18b20_records().await?;
 
-        records.extend(bme280_records
-            .into_iter()
-            .map(|r| {
-                let bme280_reading = BME280::new(r.temperature, r.pressure, r.humidity);
-                let reading = Reading::BME280(bme280_reading);
-                Record::new(r.id, r.timestamp, reading)
-            })
-        );
+        records.extend(bme280_records.into_iter().map(|r| {
+            let bme280_reading = BME280::new(r.temperature, r.pressure, r.humidity);
+            let reading = Reading::BME280(bme280_reading);
+            Record::new(r.id, r.timestamp, reading)
+        }));
 
-        records.extend(ds18b20_records
-            .into_iter()
-            .map(|r| {
-                let ds18b20_reading = DS18B20::new(r.device_name, r.raw_reading);
-                let reading = Reading::DS18B20(ds18b20_reading);
-                Record::new(r.id, r.timestamp, reading)
-            })
-        );
+        records.extend(ds18b20_records.into_iter().map(|r| {
+            let ds18b20_reading = DS18B20::new(r.device_name, r.raw_reading);
+            let reading = Reading::DS18B20(ds18b20_reading);
+            Record::new(r.id, r.timestamp, reading)
+        }));
 
         Ok(records)
     }
 
-    pub(crate) async fn get_bme280_record_by_id(&self, record_id: Uuid) -> Result<Option<Bme280Record>, sqlx::Error> {
+    pub(crate) async fn get_bme280_record_by_id(
+        &self,
+        record_id: Uuid,
+    ) -> Result<Option<Bme280Record>, sqlx::Error> {
         let record = sqlx::query_as::<_, Bme280Record>(r#"SELECT id, temperature, pressure, humidity, timestamp FROM records.bme280 WHERE id = $1"#)
             .bind(record_id)
             .fetch_optional(&self.db_pool)
@@ -139,34 +151,53 @@ impl Repository {
     }
 
     pub(crate) async fn get_all_bme280_records(&self) -> Result<Vec<Bme280Record>, sqlx::Error> {
-        let records = sqlx::query_as::<_, Bme280Record>(r#"SELECT id, temperature, pressure, humidity, timestamp FROM records.bme280"#).fetch_all(&self.db_pool).await?;
+        let records = sqlx::query_as::<_, Bme280Record>(
+            r#"SELECT id, temperature, pressure, humidity, timestamp FROM records.bme280"#,
+        )
+        .fetch_all(&self.db_pool)
+        .await?;
         Ok(records)
     }
 
-    pub(crate) async fn get_ds18b20_record_by_id(&self, record_id: Uuid) -> Result<Option<Ds18b20Record>, sqlx::Error> {
-        let record = sqlx::query_as::<_, Ds18b20Record>(r#"SELECT id, device_name, raw_reading, timestamp FROM records.ds18b20 WHERE id = $1"#)
-            .bind(record_id)
-            .fetch_optional(&self.db_pool)
-            .await?;
+    pub(crate) async fn get_ds18b20_record_by_id(
+        &self,
+        record_id: Uuid,
+    ) -> Result<Option<Ds18b20Record>, sqlx::Error> {
+        let record = sqlx::query_as::<_, Ds18b20Record>(
+            r#"SELECT id, device_name, raw_reading, timestamp FROM records.ds18b20 WHERE id = $1"#,
+        )
+        .bind(record_id)
+        .fetch_optional(&self.db_pool)
+        .await?;
 
         Ok(record)
     }
 
     pub(crate) async fn get_all_ds18b20_records(&self) -> Result<Vec<Ds18b20Record>, sqlx::Error> {
-        let records = sqlx::query_as::<_, Ds18b20Record>(r#"SELECT id, device_name, raw_reading, timestamp FROM records.ds18b20"#).fetch_all(&self.db_pool).await?;
+        let records = sqlx::query_as::<_, Ds18b20Record>(
+            r#"SELECT id, device_name, raw_reading, timestamp FROM records.ds18b20"#,
+        )
+        .fetch_all(&self.db_pool)
+        .await?;
         Ok(records)
     }
 
     pub(crate) async fn get_user_by_username(&self, username: &str) -> Result<User, sqlx::Error> {
-        let user = sqlx::query_as::<_, User>(r#"SELECT id, username, password FROM auth.users WHERE username = $1"#)
-            .bind(username)
-            .fetch_one(&self.db_pool)
-            .await?;
+        let user = sqlx::query_as::<_, User>(
+            r#"SELECT id, username, password FROM auth.users WHERE username = $1"#,
+        )
+        .bind(username)
+        .fetch_one(&self.db_pool)
+        .await?;
 
         Ok(user)
     }
 
-    pub(crate) async fn create_user(&self, username: &str, password: &str) -> Result<User, sqlx::Error> {
+    pub(crate) async fn create_user(
+        &self,
+        username: &str,
+        password: &str,
+    ) -> Result<User, sqlx::Error> {
         let user = User::new(username.into(), password.into()).unwrap();
         sqlx::query(r#"INSERT INTO auth.users (id, username, password) VALUES ($1, $2, $3);"#)
             .bind(user.id())
@@ -180,18 +211,24 @@ impl Repository {
 
     pub(crate) async fn create_api_key(&self, api_key: &ApiKey) -> Result<(), sqlx::Error> {
         let owner = self.get_user_by_username(api_key.owner()).await?;
-        sqlx::query(r#"INSERT INTO auth.api_keys (id, name, owner_id, token) VALUES ($1, $2, $3, $4);"#)
-            .bind(api_key.id())
-            .bind(api_key.name())
-            .bind(owner.id())
-            .bind(api_key.token())
-            .execute(&self.db_pool)
-            .await?;
+        sqlx::query(
+            r#"INSERT INTO auth.api_keys (id, name, owner_id, token) VALUES ($1, $2, $3, $4);"#,
+        )
+        .bind(api_key.id())
+        .bind(api_key.name())
+        .bind(owner.id())
+        .bind(api_key.token())
+        .execute(&self.db_pool)
+        .await?;
         Ok(())
     }
 
-    pub(crate) async fn get_api_key_by_token(&self, token_value: String) -> Result<ApiKey, sqlx::Error> {
-        let token = sqlx::query_as::<_, ApiKey>(r#"
+    pub(crate) async fn get_api_key_by_token(
+        &self,
+        token_value: String,
+    ) -> Result<ApiKey, sqlx::Error> {
+        let token = sqlx::query_as::<_, ApiKey>(
+            r#"
 SELECT
     keys.id, keys.name, users.username as owner, token
 FROM
@@ -199,29 +236,33 @@ FROM
     JOIN auth.users users ON keys.owner_id = users.id
 WHERE
     token = $1;
-"#)
-            .bind(token_value)
-            .fetch_one(&self.db_pool)
-            .await?;
+"#,
+        )
+        .bind(token_value)
+        .fetch_one(&self.db_pool)
+        .await?;
         Ok(token)
     }
 
     pub(crate) async fn list_api_keys(&self) -> Result<Vec<ApiKey>, sqlx::Error> {
-        let records = sqlx::query_as::<_, ApiKey>(r#"
+        let records = sqlx::query_as::<_, ApiKey>(
+            r#"
 SELECT
     keys.id, name, username as owner, token
 FROM
     auth.api_keys keys
     JOIN auth.users users ON keys.owner_id = users.id;
-            "#)
-            .fetch_all(&self.db_pool).await?;
+            "#,
+        )
+        .fetch_all(&self.db_pool)
+        .await?;
         Ok(records)
     }
 
     fn limit(max_length: Option<usize>) -> usize {
         match max_length {
-            None => {DEFAULT_LIMIT}
-            Some(max_length) => {std::cmp::min(max_length, HARD_LIMIT)}
+            None => DEFAULT_LIMIT,
+            Some(max_length) => std::cmp::min(max_length, HARD_LIMIT),
         }
     }
 }
